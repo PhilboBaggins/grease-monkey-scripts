d48b61534795c8810e92174e07ee3d89eceebfdd
Phil <PhilboBaggins@users.noreply.github.com>
2014-02-12 02:52:01 +1100

    Improve automatic-word-substitution.user.js by using replaceText jQuery plugin

--- automatic-word-substitution.user.js
+++ automatic-word-substitution.user.js
@@ -9,43 +9,50 @@
 
 console.time('Automatic word substitution execution time');
 
-$('body').children().each(function() {
-    var newContent = $(this).html()
-        // Farscape swear words
-        .replace('Fuck', 'Frell')
-        .replace('fuck', 'frell')
-        .replace('Fuckwit', 'Frellnick')
-        .replace('fuckwit', 'frellnick')
-        .replace('Slut', 'Tralk') // or is it Trel'ik
-        .replace('slut', 'tralk')
-        .replace('Whore', 'Tralk')
-        .replace('whore', 'tralk')
-        .replace('Testicles', 'Mivonks')
-        .replace('testicles', 'Mivonks')
-        .replace('Crap', 'Dren')
-        .replace('crap', 'dren')
-        .replace('Shit', 'Dren')
-        .replace('shit', 'Ddren')
-        .replace('Hell', 'Hezmata')
-        .replace('hell', 'hezmata')
-        .replace('Hell', 'Hezmata')
-        .replace('hell', 'hezmata')
-        .replace('Hell', 'Yotz')
-        .replace('hell', 'yotz')
-        .replace('Ass', 'Eema')
-        .replace('ass', 'eema')
-        .replace('Arse', 'Eema')
-        .replace('arse', 'eema')
-        .replace('Crazy', 'Fahrbot')
-        .replace('crazy', 'fahrbot')
-        .replace('Insane', 'Fahrbot')
-        .replace('insane', 'fahrbot')
-        .replace('Demented', 'Fahrbot')
-        .replace('demented', 'fahrbot')
-        // Futurama
-        .replace('ask you a question', 'axe you a question')
-        ;
-    $(this).html(newContent);
-});
+/*
+ * jQuery replaceText - v1.1 - 11/21/2009
+ * http://benalman.com/projects/jquery-replacetext-plugin/
+ *
+ * Copyright (c) 2009 "Cowboy" Ben Alman
+ * Dual licensed under the MIT and GPL licenses.
+ * http://benalman.com/about/license/
+ *
+ * Downloaded from http://github.com/cowboy/jquery-replacetext/raw/master/jquery.ba-replacetext.min.js
+ */
+(function($){$.fn.replaceText=function(b,a,c){return this.each(function(){var f=this.firstChild,g,e,d=[];if(f){do{if(f.nodeType===3){g=f.nodeValue;e=g.replace(b,a);if(e!==g){if(!c&&/</.test(e)){$(f).before(e);d.push(f)}else{f.nodeValue=e}}}}while(f=f.nextSibling)}d.length&&$(d).remove()})}})(jQuery);
+
+$('*')
+    // Farscape swear words
+    .replaceText('Fuck', 'Frell')
+    .replaceText('fuck', 'frell')
+    .replaceText('Fuckwit', 'Frellnick')
+    .replaceText('fuckwit', 'frellnick')
+    .replaceText('Slut', 'Tralk') // or is it Trel'ik
+    .replaceText('slut', 'tralk')
+    .replaceText('Whore', 'Tralk')
+    .replaceText('whore', 'tralk')
+    .replaceText('Testicles', 'Mivonks')
+    .replaceText('testicles', 'Mivonks')
+    .replaceText('Crap', 'Dren')
+    .replaceText('crap', 'dren')
+    .replaceText('Shit', 'Dren')
+    .replaceText('shit', 'dren')
+    .replaceText('Hell', 'Hezmata')
+    .replaceText('hell', 'hezmata')
+    //.replaceText('Hell', 'Yotz')
+    //.replaceText('hell', 'yotz')
+    .replaceText('Ass', 'Eema')
+    .replaceText('ass', 'eema')
+    .replaceText('Arse', 'Eema')
+    .replaceText('arse', 'eema')
+    .replaceText('Crazy', 'Fahrbot')
+    .replaceText('crazy', 'fahrbot')
+    .replaceText('Insane', 'Fahrbot')
+    .replaceText('insane', 'fahrbot')
+    .replaceText('Demented', 'Fahrbot')
+    .replaceText('demented', 'fahrbot')
+    // Futurama
+    .replaceText('ask you a question', 'axe you a question')
+    ;
 
 console.timeEnd('Automatic word substitution execution time');

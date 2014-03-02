7a3e951290f029ec1fc8e42b96b6abed47470fd3
Phil <PhilboBaggins@users.noreply.github.com>
2014-03-02 18:02:48 +1100

    Add hackaday-comments-fixer.user.js

---
+++ hackaday-comments-fixer.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name        Hack a Day comment fixer
+// @namespace   hack-a-day-comment-fixer
+// @description Hide those annoying "This is not a hack" comments on hackaday.com
+// @include     http://hackaday.com/*
+// @author      Phil
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+// Test page: http://hackaday.com/2014/02/04/manual-bitcoin-transactions/
+
+function makeid(length)
+{
+    // https://stackoverflow.com/questions/1349404/generate-a-string-of-5-random-characters-in-javascript
+
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 10; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+function ReplaceCommentBox(commentBox) {
+    var originalContent = commentBox.innerHTML;
+    function restoreComment() {
+        commentBox.innerHTML = originalContent;
+    }
+    var idForNewButton = 'restore-not-a-hack-comment-button-' + makeid();
+    commentBox.innerHTML = '<p>"not a hack" comment. <button id="' + idForNewButton + '">Click here</button> to restore it</p>';
+    var button = document.getElementById(idForNewButton);
+    button.addEventListener('click', restoreComment, true);
+}
+
+var commentBoxes = document.getElementsByClassName('comment-content');
+
+for (var i = 0; i < commentBoxes.length; i++) {
+    if (commentBoxes[i].innerHTML.toLowerCase().indexOf('not a hack') !== -1) {
+        ReplaceCommentBox(commentBoxes[i]);
+    }
+}
